95fc920765d7583ce47959a5e66b48ec7e040804
Tushar Bhandarwar <tusharbhandarwar01@gmail>
2026-06-24 08:36:07 +0530

    Added data.sql

---
+++ Data.sql
@@ -0,0 +1,22 @@
+INSERT INTO employees (emp_id, emp_name, department, salary)
+VALUES
+(1, 'Rahul', 'IT', 50000),
+(2, 'Priya', 'HR', 45000),
+(3, 'Amit', 'Finance', 55000),
+(4, 'Sneha', 'IT', 60000),
+(5, 'Vikram', 'Sales', 48000),
+(6, 'Neha', 'Marketing', 52000),
+(7, 'Arjun', 'IT', 65000),
+(8, 'Pooja', 'HR', 47000),
+(9, 'Karan', 'Finance', 58000),
+(10, 'Anjali', 'Sales', 51000);
+
+
+
+INSERT INTO departments (dept_id, dept_name)
+VALUES
+(101, 'IT'),
+(102, 'HR'),
+(103, 'Finance'),
+(104, 'Sales'),
+(105, 'Marketing');
